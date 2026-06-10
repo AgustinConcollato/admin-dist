acee2617bcf4e14417e29d6908da08dff675a67c
Agustín Concollato <agustinconcollato@gmail.com>
2026-06-09 21:11:57 -0300

    .

---
+++ assets/parseApiError-CNK4aNRs.js
@@ -0,0 +1,1 @@
+function r(e){return!e||typeof e!="object"?{fieldErrors:null,message:String(e)}:{fieldErrors:e.errors??null,message:e.error??e.message??"Ocurrió un error inesperado."}}export{r as p};
--- assets/priceListService-CcWh7u5Y.js
+++
@@ -1,1 +0,0 @@
-import{Y as e}from"./index-Dx9Z9SW7.js";class t{async get(){return e("/price-lists",{method:"GET"})}}export{t as P};
