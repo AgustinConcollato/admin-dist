3e1b88af0e77add6b094a70ae81450398754daaa
Agustín Concollato <agustinconcollato@gmail.com>
2026-07-11 03:28:23 -0300

    .

--- assets/priceListService-C9z5rpI2.js
+++
@@ -1,1 +0,0 @@
-import{Y as e}from"./index-D5qRxaRg.js";class t{async get(){return e("/price-lists",{method:"GET"})}}export{t as P};
---
+++ assets/priceListService-vmqt1Q_-.js
@@ -0,0 +1,1 @@
+import{Y as e}from"./index-5FgPKrmM.js";class t{async get(){return e("/price-lists",{method:"GET"})}}export{t as P};
